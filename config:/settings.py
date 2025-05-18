f72d458e5b343b744eca1a9139968b275dd7b685
MeowEyelash <vadimsteepkicul@mail.ru>
2025-05-18 22:59:01 +0300

    Update settings.py

--- config:/settings.py
+++ config:/settings.py
@@ -1,4 +1,4 @@
 TELEGRAM_TOKEN = "7881390086:AAHPfv0SvY1ljBOwMSzVQjryCVgzfPoI0Xk"  
-ADMIN_ID = 647251759 
-OPENAI_KEY = "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"  # Для теста (в продакшене получите на platform.openai.com)
+ADMIN_ID = "647251759"
+OPENAI_KEY = "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"
 SENTRY_DSN = ""  
